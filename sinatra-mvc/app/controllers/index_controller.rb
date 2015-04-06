a02ccef4b18d6f1d244ea570b46ad1da861ddd5a
JoeiV <joei.lynyrd@gmail.com>
2015-04-06 11:34:13 +0000

    Reinitialize

---
+++ sinatra-mvc/app/controllers/index_controller.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+set :bind, '0.0.0.0'
+
+get '/' do
+    erb :index
+end
+
+get '/anagrams/:word' do
+    @word= params[:word]
+    @anagrams = Word.find_anagram(@word)
+    erb :show
+end
+
+post '/' do
+  @word = params[:word]
+  redirect "/anagrams/#{@word}"
+end
