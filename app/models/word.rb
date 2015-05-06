528cbd7bced8b44a0bb6bee8850783126bdae28a
JoeiV <joei.lynyrd@gmail.com>
2015-05-06 15:26:48 +0000

    Added editing

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,7 @@
 class Word < ActiveRecord::Base
     
+    before_save :add_letters
+    
     def add_letters
         characters = self.text.chars
         alphabetized_characters = characters.sort
